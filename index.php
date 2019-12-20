ac1c197666109ed8622906016627811dafe0e19b
khaerana <59088767+khaerana@users.noreply.github.com>
2019-12-20 21:36:11 +0800

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,24 @@
 	<title>Aplikasi Tugas Cloud Computing</title>
 </head>
 <body>
-	<h1>Hello World!</h1>
+	<h1>Selamat Datang di Websiteku</h1>
+	
+	<table align="center">
+		<tr>
+			<td>Nama</td>
+			<td>:</td>
+			<td>Khaerana</td>
+		</tr>
+		<tr>
+			<td>Nim</td>
+			<td>:</td>
+			<td>216 280 005</td>
+		</tr>
+		<tr>
+			<td>Kelas</td>
+			<td>:</td>
+			<td>Informatika A</td>
+		</tr>
+	</table>
 </body>
 </html>
